9a3e613005139af18d216bf8f2d700b34c897e86
Zeger Hoogeboom <zeger.hoogeboom@gmail.com>
2017-03-23 23:18:12 +0100

    Added a "MappingCreator" script, which is capable of creating and reading mappings of transactions and inputs.

---
+++ MappingCreator.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+
+class MappingCreator:
+    def __init__(self):
+        self.mapping = {}
+        self.counter = 0
+        self.txMapping = {}
+        self.txCounter = 0
+        pass
+
+    def createMapping(self):
+        self.__readFile()
+        self.__populateMapping()
+        self.__writeMappings()
+
+    def readInputMapping(self):
+        return self.__readMapping("inputs")
+
+    def readTxMapping(self):
+        return self.__readMapping("tx")
+
+    def __readFile(self):
+        self.full_data = pd.read_csv("sample_tx.txt", names=['tx', 'inputs'])
+        for ix, value in self.full_data['inputs'].iteritems():
+            self.full_data['inputs'][ix] = value.split(';')
+
+    def __compressInputValue(self, input):
+        if input not in self.mapping:
+            self.mapping[input] = self.counter
+            self.counter += 1
+
+    def __compressTxValue(self, tx):
+        if tx not in self.txMapping:
+            self.txMapping[tx] = self.txCounter
+            self.txCounter += 1
+
+    def __populateMapping(self):
+        for ix, value in self.full_data['inputs'].iteritems():
+            for input in value:
+                self.__compressInputValue(input)
+
+        for ix, value in self.full_data['tx'].iteritems():
+            self.__compressTxValue(value)
+
+    def __writeMappings(self):
+        with open('mappings/inputs.pkl', 'wb') as f:
+            pickle.dump(self.mapping, f, pickle.HIGHEST_PROTOCOL)
+        with open('mappings/tx.pkl', 'wb') as f:
+            pickle.dump(self.txMapping, f, pickle.HIGHEST_PROTOCOL)
+
+    def __readMapping(self, file):
+        with open('mappings/'+file+'.pkl', 'rb') as f:
+            return pickle.load(f)
+
+
+print MappingCreator().readInputMapping()
+print MappingCreator().readTxMapping()
