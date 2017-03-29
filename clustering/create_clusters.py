ffa725767d49e6ebb1cd5c0a1c562d6a7f3bcb7b
Zeger Hoogeboom <zeger.hoogeboom@gmail.com>
2017-03-29 23:37:30 +0200

    added support for command line arguments

--- clustering/create_clusters.py
+++ clustering/create_clusters.py
@@ -1,20 +1,27 @@
+import sys
+
 from UnionFind import UnionFind
 
-family = UnionFind()
+data_file = "../mappings/mapped.txt"
+output_file = "clusters.txt"
+if len(sys.argv) >= 2: data_file = sys.argv[1]
+if len(sys.argv) >= 3: output_file = sys.argv[2]
+
+users = UnionFind()
 
 print "Reading mapped data..."
-mapped_data = open("../mappings/mapped.txt", 'r')
+mapped_data = open(data_file, 'r')
 
 print "Going through data..."
 for line in mapped_data:
     line = line.replace("\n", "")
     inputs = line.split(',')[1:]
-    family.union(*inputs)
+    users.union(*inputs)
 print "Done creating clusters..."
 
 print "Getting clusters..."
-clusters = family.get_clusters()
-cluster_file = open("clusters.txt", "a")
+clusters = users.get_clusters()
+cluster_file = open(output_file, "a")
 
 print "Writing clusters..."
 for k, v in clusters.iteritems():
